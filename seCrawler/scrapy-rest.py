7f97fbf78f33e6e5a726affdb8e5408ca9efe14e
Alon Weinberg <alonwein@gmail.com>
2016-08-15 12:37:45 +0300

    add support for se parameter, though the class support only Bing at the moment

--- seCrawler/scrapy-rest.py
+++ seCrawler/scrapy-rest.py
@@ -17,16 +17,13 @@ def crawl(request):
     arg1=''
     if 'keyword' in request.args:
         kwargs['keyword']=request.args['keyword'][0]
-        arg1='keyword='+request.args['keyword'][0]
     else:
         return "keyword is required"
     if 'pages' in request.args:
         pages=request.args['pages'][0]
         kwargs['pages'] = request.args['pages'][0]
-        arg1 += 'pages=' + request.args['pages'][0]
     if 'se' in request.args:
-        se=request.args['se'][0]
-        #args['se'] = request.args['se'][0]
+        kwargs['se'] = request.args['se'][0]
     runner = seCrawler()
     spider = restSpider(**kwargs)
     deferred = runner.crawl(spider,**kwargs)
